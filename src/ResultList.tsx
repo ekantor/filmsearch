ab6088519f253605e414b3ce04c65b667c1cc042
Kántor Endre <kaendre@gmail.com>
2019-05-15 20:16:49 +0200

    margin

--- src/ResultList.tsx
+++ src/ResultList.tsx
@@ -4,12 +4,21 @@ import ResultItem from './ResultItem';
 import SearchState from './SearchState';
 import CircularProgress from '@material-ui/core/CircularProgress';
 import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/styles';
+
+const useStyles = makeStyles({
+	topMargin: {
+		marginTop: '2rem'
+	}
+});
 
 const ResultList = (props: { state: SearchState, items: MovieInfo[] }) => {
+	const classes = useStyles();
+
 	if (props.state === SearchState.Initial) {
 		return null;
 	} else if (props.state === SearchState.Fetching) {
-		return <CircularProgress style={{ marginTop: '1rem' }}/>;
+		return <CircularProgress className={classes.topMargin}/>;
 	} else if (props.state === SearchState.Found) {
 		return (
 			<div style={{width: '100%'}}>
@@ -19,9 +28,9 @@ const ResultList = (props: { state: SearchState, items: MovieInfo[] }) => {
 			</div>
 		);
 	} else if (props.state === SearchState.NotFound) {
-		return <Typography variant='h6'>No results found.</Typography>;
+		return <Typography variant='h6' className={classes.topMargin}>No results found.</Typography>;
 	} else {
-		return <Typography variant='h6'>An error occurred during search.</Typography>;
+		return <Typography variant='h6' className={classes.topMargin}>An error occurred during search.</Typography>;
 	}
 }
 
